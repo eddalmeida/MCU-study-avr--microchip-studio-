865dc80aaaa271955c5d998235043ccedeee81a3
edson macedo de almeida <144967252+eddalmeida@users.noreply.github.com>
2025-09-26 18:31:03 -0300

    alterando nome macro

--- muxDisplay7seg/muxDisplay7seg/main.c
+++ muxDisplay7seg/muxDisplay7seg/main.c
@@ -3,7 +3,7 @@
 #include "avr/pgmspace.h"                                          //Biblioteca para escrever na flash
 #include <util/delay.h>                                            //Biblioteca para utilizar delay
 
-#define ready(PINx, Px) ((PINx & (1 << Px)) >> Px)                 //macro para ler botão e retornar 1 ou 0
+#define read(PINx, Px) ((PINx & (1 << Px)) >> Px)                 //macro para ler botão e retornar 1 ou 0
 #define set_bit(PORTx, Px) (PORTx |= (1 << Px))                    //macro para colocar pino como alto
 #define clr_bit(PORTx, Px) (PORTx &= !(1 << Px))                   //macro para colocar pino como baixo
 
@@ -35,8 +35,8 @@ int main(void)
     while (1) 
     {
 		
-		unsigned botao1 = ready(PINB, btn1);                          //armazena valor do botao1 
-		unsigned botao2 = ready(PINB, btn2);                          //amarzena estado anterior do botão para ler apenas 1 vez
+		unsigned botao1 = read(PINB, btn1);                          //armazena valor do botao1 
+		unsigned botao2 = read(PINB, btn2);                          //amarzena estado anterior do botão para ler apenas 1 vez
 		
 		
 		if (botao1 == 0 && estadoant1 == 1)                           //leitura botão de decrementar
